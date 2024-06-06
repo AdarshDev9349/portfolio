02de2231a8bf8510e0ae7b37386496002c693c17
Adarshdev <adarshdev9349@gmail.com>
2024-06-06 22:35:31 +0530

    new-added

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,20 +1,27 @@
+import image from './images/character.svg'
+
 const timeline = () => {
     return (
-        <div className="time-line">
-           <hr />
-           <div className="box"></div>
-           <hr />
-           <div className="box"></div>
-           <hr />
-           <div className="box"></div>
-           <hr />
-           <div className="box"></div>
-           <hr />
+        
+<>
+<img src={image} style={{position:'relative',zIndex:'2',top:'430px',width:'180px'}} alt="" />
+<div className="time-line"> 
+         
+         <hr />
+         <div className="box">1</div>
+         <hr />
+         <div className="box">2</div>
+         <hr />
+         <div className="box">3</div>
+         <hr />
+         <div className="box">4</div>
+         <hr />
 
 
 
 
-        </div>
+      </div>
+</>
 
     );
   };
