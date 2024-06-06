070f4573e39adeb3e4ba40b8e018512663ca4aae
Adarshdev <adarshdev9349@gmail.com>
2024-06-06 20:15:02 +0530

    new-added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import Navbar from './components/navbar'
 import Homepage from './components/landing'
 import Hackathon from './components/hackathon'
+import renvnza from './components/logo/renvnza.svg'
+import mulearn from './components/logo/mulearn.svg'
+import gdsc from './components/logo/gdsc.png'
+import ieee from './components/logo/ieee.png'
+import Timeline from './components/timeline'
 import './index.css'
 
 function App() {
@@ -12,9 +17,14 @@ function App() {
   <Navbar/>
   <Homepage/>
   <Hackathon/>
-
-
-
+<div className="logos">
+   <img src={renvnza} alt="Renvnza" />
+   <img src={mulearn} alt="Mulearn ucek" />
+   <img src={gdsc}  alt="GDSC Ucek" />
+   <img src={ieee} alt="IEEE Ucek" />
+</div>
+ 
+  <Timeline/>
 
  </div>
   )
---
+++ src/components/timeline.jsx
@@ -0,0 +1,22 @@
+const timeline = () => {
+    return (
+        <div className="time-line">
+           <hr />
+           <div className="box"></div>
+           <hr />
+           <div className="box"></div>
+           <hr />
+           <div className="box"></div>
+           <hr />
+           <div className="box"></div>
+           <hr />
+
+
+
+
+        </div>
+
+    );
+  };
+  
+  export default timeline ;
