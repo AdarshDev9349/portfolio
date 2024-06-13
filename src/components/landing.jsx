19e1492fce140b9d5a3279a55bd7c56dcac1fb2d
a-run-kumar-d <arunkumarpkmn@example.com>
2024-06-13 11:10:08 +0530

    Landing page

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -3,18 +3,26 @@ import { Link } from "react-router-dom";
 const Homepage = () => {
   return (
     <div className="home">
-      
-      <motion.h1
-        animate={{ x: 45 }}
-        transition={{ ease: "easeOut", duration: 2 }}
-      >
-        Mine the problem Craft <br /> the Solution
-      </motion.h1>
-      <p>Hackathon </p>
+      <div className="herotext">
+        <motion.h1
+          className="Orginal"
+          animate={{ x: 45 }}
+          transition={{ ease: "easeOut", duration: 2 }}
+        >
+          Mine the Problem <br /> Craft the Solution
+        </motion.h1>
+        <motion.h1
+          className="Wrapper"
+          animate={{ x: 45 }}
+          transition={{ ease: "easeOut", duration: 2 }}
+        >
+          Mine the Problem <br /> Craft the Solution
+        </motion.h1>
+      </div>
+      {/* <h3 className="subtext">Hhehehehehehehe</h3> */}
       <div className="button-container">
         <button className="button2 register">Register Now</button>
         <Link to={"/sponsor-us"}>
-          {" "}
           <button className="button1">Sponsor us</button>
         </Link>
       </div>
